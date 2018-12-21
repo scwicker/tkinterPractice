b282b633b437543697bdeecbb84d9f655da5f088
wickerss <wickerss@oregonstate.edu>
2018-12-21 01:32:18 -0500

    added clear field buttons

--- main.py
+++ main.py
@@ -17,10 +17,22 @@ def dothemath ():
 	productLabel.grid(row=3, column=3)
 	return productResult
 
+def clearField(tInput):
+	tInput.delete(0,END)
+
+num1 = IntVar()
+num2 = IntVar()
+
+textinput1 = Entry(outerShell, textvariable=num1)
+textinput2 = Entry(outerShell, textvariable=num2)
+	
+
 label_1 = Label(outerShell, text = "Enter first number")
 label_2 = Label(outerShell, text = "Enter second number")
 label_submit = Label(outerShell, text = "Click submit for results")
 submit_button = Button(outerShell, text = "Submit", command=dothemath)
+clear1_button = Button(outerShell, text = "Clear", command= lambda: clearField(textinput1))
+clear2_button = Button(outerShell, text = "Clear", command= lambda: clearField(textinput2))
 label_results = Label(outerShell, text = "The product of your two numbers is:")
 
 label_1.grid(row=0, column = 0)
@@ -29,13 +41,12 @@ label_submit.grid(row=2, column = 0)
 submit_button.grid(row=2, column =1)
 label_results.grid(row=3, column = 0)
 
-num1 = IntVar()
-num2 = IntVar()
 
-textinput1 = Entry(outerShell, textvariable=num1)
-textinput2 = Entry(outerShell, textvariable=num2)
 textinput1.grid(row = 0, column = 1)
 textinput2.grid(row = 1, column = 1)
+clear1_button.grid(row = 0, column =2)
+clear2_button.grid(row = 1, column =2)
+
 
 
 	
